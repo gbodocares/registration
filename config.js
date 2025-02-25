948c5670ee161311532b44047a070faf63bf48e9
gbodocares <gbodocares17@gmail.com>
2025-02-25 09:20:57 +0100

    edited information

--- config.js
+++ config.js
@@ -1,10 +1,21 @@
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCzeXvcgkgLgQu1Pj2qce4wANvbe7ChWO4",
+//   authDomain: "gclc-eabb4.firebaseapp.com",
+//   databaseURL: "https://gclc-eabb4-default-rtdb.firebaseio.com",
+//   projectId: "gclc-eabb4",
+//   storageBucket: "gclc-eabb4.appspot.com",
+//   messagingSenderId: "588542989378",
+//   appId: "1:588542989378:web:ee83a42419ccaeac0249db"
+// };
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAUENQYbCNyU-xCptpbB1aRMUyTw5CVtw0",
-    authDomain: "gclc-f7cbb.firebaseapp.com",
-    projectId: "gclc-f7cbb",
-    storageBucket: "gclc-f7cbb.appspot.com",
-    messagingSenderId: "273312236534",
-    appId: "1:273312236534:web:d5fb06d9ef6fe324b47dd7"
+    apiKey: "AIzaSyCzeXvcgkgLgQu1Pj2qce4wANvbe7ChWO4",
+    authDomain: "gclc-eabb4.firebaseapp.com",
+    databaseURL: "https://gclc-eabb4-default-rtdb.firebaseio.com",
+    projectId: "gclc-eabb4",
+    storageBucket: "gclc-eabb4.appspot.com",
+    messagingSenderId: "588542989378",
+    appId: "1:588542989378:web:ee83a42419ccaeac0249db"
 };
 
 firebase.initializeApp(firebaseConfig);
--- slip.js
+++ slip.js
@@ -61,7 +61,7 @@ firebase.auth().onAuthStateChanged((user) => {
                         </p>
                         <p>
                          ${userData.address} <br>
-                         ${userData.community}
+                         ${userData.community},
                          ${userData.lga}<br>
                         <a href="#">
                         <span class="__cf_email__" data-cfemail="98f2f7f6f6e1fcfdfefed8fff5f9f1f4b6fbf7f5">${userData.email}</span>
@@ -74,73 +74,84 @@ firebase.auth().onAuthStateChanged((user) => {
                         <div class="line-items">
                         <div class="headers clearfix">
                         <div class="row">
-                        <div class="col-xs-4" style="color:teal; font-weight:bold;">Course(s)</div>
-                        <div class="col-xs-5 text-right" style="color:teal; font-weight:bold;">Choice(s)</div>
+                        <div class="col-xs-4" style="color:teal; font-weight:bold;">Preferred Course</div>
+                        
                         </div>
                         </div>
                         <div class="items" style="margin-top: -10px;">
                         <div class="row item">
-                        <div class="col-xs-4 desc" style="font-weight: bold;">
-                        ${userData.firstcourse}
+                        <div class="col-xs-4 desc" style="font-weight: bold; color: teal;">
+                        1. ${userData.firstcourse}
                         </div>
                         <div class="col-xs-5 qty text-right" style="font-weight: bold;">
                         First choice
                         </div>
                        
                         </div>
+                        
+
+                        <div>
+
+                        <div class="items" style="margin-top: -10px;">
                         <div class="row item">
-                        <div class="col-xs-4 desc" style="font-weight: bold;">
-                         ${userData.secondcourse}
+                        <div class="col-xs-4 desc" style="font-weight: bold; color: teal;">
+                        2. ${userData.secondcourse}
                         </div>
                         <div class="col-xs-5 qty text-right" style="font-weight: bold;">
                         Second choice
                         </div>
-                      
+                       
                         </div>
+                        
 
                         <div>
+                        <b><p>Instructions</p></b>
+                        <ol>
+                            <li><b>Please ensure you check your course(s) and attend the screening date.</b></li>
+                            <li><b>Please come with your acknowledgement slip to the screening venue.</b></li>
+                        </ol>
                         <b><h5>Screening Dates</h5></b>
-                    <table style="width:100%;">
+                    <table style="width:100%; ">
                         <tr style=" border: 1px solid #000;">
-                            <th>Courses</th>
-                            <th>Date</th>
-                            <th>Time</th>
+                            <th style=" border: 1px solid #000; padding-left: 10px;">Courses</th>
+                            <th style=" border: 1px solid #000; padding-left: 10px;">Date</th>
+                            <th style=" border: 1px solid #000; padding-left: 10px;">Time</th>
                         </tr>
 
                         <tr style=" border: 1px solid #000;">
-                            <td>
-                                <ul>
+                            <td style=" border: 1px solid #000;">
+                                <ol style=" margin-left:-10px; font-weight: bold;">
                                     <li>Make-up</li>
                                     <li>Tailoring</li>
                                     <li>Nails Decoration</li>
-                                </ul>
+                                </ol>
                             </td>
-                            <td >Mon 24th, Mar 2025</td>
-                            <td >10:00am - 4:00pm</td>
+                            <td style=" border: 1px solid #000; padding-left: 10px;">Mon 24th, Mar 2025</td>
+                            <td style=" border: 1px solid #000; padding-left: 10px;">10:00am - 4:00pm</td>
                         </tr>
 
                         <tr style=" border: 1px solid #000;">
-                            <td>
-                                <ul>
-                                    <li>Photography</li>
-                                    <li>Videography</li>
+                            <td style=" border: 1px solid #000;">
+                                <ol style=" margin-left:-10px; font-weight: bold;">
                                     <li>Video Editing</li>
-                                </ul>
+                                    <li>Videography</li>
+                                    <li>Photography</li>
+                                </ol>
                             </td>
-                            <td>Tue 25th, Mar 2025</td>
-                            <td>10:00am - 4:00pm</td>
+                            <td style=" border: 1px solid #000; padding-left: 10px;">Tue 25th, Mar 2025</td>
+                            <td style=" border: 1px solid #000; padding-left: 10px;">10:00am - 4:00pm</td>
                         </tr>
 
                         <tr style=" border: 1px solid #000;">
-                            <td>
-                                <ul>
+                            <td style=" border: 1px solid #000;">
+                                <ol style=" margin-left:-10px; font-weight: bold;">
                                     <li>Web Design</li>
-                                    <li>Backend Development</li>
-                                    <li>UI/UX Design</li>
-                                </ul>
+                                    <li>UI/UX DESIGN</li>
+                                    <li>Cyber Security</li>
+                                </ol>
                             </td>
-                            <td>Wed 26th, Mar 2025</td>
-                            <td>10:00am - 4:00pm</td>
+                            <td style=" border: 1px solid #000; padding-left: 10px;">Wed 26th, Mar 2025</td>
+                            <td style=" border: 1px solid #000; padding-left: 10px;">10:00am - 4:00pm</td>
                         </tr>
                     </table>
                 </div>
