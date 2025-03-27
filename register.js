8c4c95acb3b84714ca28767d528428df5e7d284e
gbodocares <gbodocares17@gmail.com>
2025-03-27 17:50:04 +0100

    edited info

--- register.js
+++ register.js
@@ -71,6 +71,8 @@ function registerMe(){
                         }
                     )
                 })
+
+               
                 
                 swal({
                     title: "Registration",
